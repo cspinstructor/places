1a3c178a19018727c284fcbd776bd378fb2cf63c
paulchin <paulchin20@yahoo.com>
2018-05-29 16:34:09 +0800

    Added get lat-lng

--- app.js
+++ app.js
@@ -14,6 +14,31 @@ server.get('/', (req, res) => {
   res.render('home.hbs');
 });
 
+server.get('/form', (req, res) => {
+  res.render('form.hbs');
+});
+
+server.post('/getplaces', (req, res) => {
+  const addr = req.body.address;
+  const locationReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyAn7h3tsW_p0md5iISNFzLcJDoRGRgjWPg`;
+
+  axios.get(locationReq).then((response) => {
+    const locationData = {
+      addr: response.data.results[0].formatted_address,
+      lat: response.data.results[0].geometry.location.lat,
+      lng: response.data.results[0].geometry.location.lng,
+    }
+    res.status(200).send(JSON.stringify(locationData));
+  })
+  .catch((error) => {
+    res.status(200).send('ERRO');
+  });
+
+
+  //const result = { address: addr };
+  //res.render('result.hbs',result);
+});
+
 server.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
