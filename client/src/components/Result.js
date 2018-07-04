7ed48715ad06e40c56adcaf4634a48c22cdd54e6
paulchin <paulchin20@yahoo.com>
2018-07-04 19:15:51 +0800

    Added striped table

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -1,16 +1,23 @@
 import React from 'react';
-
 const Result = props => {
   //console.log(props.list.length);
 
   return (
-    <div>
-      <ul>
-        <th>Name</th>
-        {props.list.map(result => {
-          return <li>{result.name}</li>;
-        })}
-      </ul>
+    <div row container-fluid>
+      <table className="table table-striped">
+        <tbody>
+          <tr>
+            <th>Name</th>
+          </tr>
+          {props.list.map(result => {
+            return (
+              <tr>
+                <td>{result.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
