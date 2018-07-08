7c32fdd5dcbce87147d813d776a43a20983c7b8a
paulchin <paulchin20@yahoo.com>
2018-07-09 03:17:19 +0800

    commented out global.promise

--- Place.js
+++ Place.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //const db = 'mongodb://localhost:27017/placesapp';
 const db = 'mongodb://paulc:abc123@ds117701.mlab.com:17701/placesapp';
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 
 mongoose
   .connect(db)
